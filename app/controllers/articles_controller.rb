561f1a5a44b878b27bb45bda2ef52e8aa67c0c68
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2019-02-04 17:37:41 -0500

    per step 5.10, added error handling to new article path and template.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,18 +8,23 @@ class ArticlesController < ApplicationController
   end
   
   def new
+    @article = Article.new
   end
 
   def create
-    @aritcle = Article.new(params[:article])
+    @aritcle = Article.new(article_params) #(params[:article])
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      # render allows the @article object to pass back to the new template when it's rendered, within the same request as the form submission
+      render 'new'
+    end
   end
 
-  # private
-  #   def article_params
-  #     params.require(:article).permit(:title, :text)
-  #   end
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  # ensures that all articles will have a title at least 5 characters long
+  validates :title, presence: true, length: { minimum: 5 }
 end
