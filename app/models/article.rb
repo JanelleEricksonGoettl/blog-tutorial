db59b6ebdd1e25fc35d171ac5416fe37ae7d22fe
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2019-02-05 14:33:31 -0500

    per step 6.3, added associations for comments on aticles model, added comments resources nested under articles resources.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   # ensures that all articles will have a title at least 5 characters long
   validates :title, presence: true, length: { minimum: 5 }
 end
